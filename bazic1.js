92faa718fc64b8c103220ed1c46ee0351a167e99
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-06-04 01:58:41 +0300

    Create bazic1.js

---
+++ bazic1.js
@@ -0,0 +1,269 @@
+
+// challenge 1
+// test
+/* tect 
+commit
+*/
+
+//challenge 2
+var myName;
+
+//challenge 3
+// Setup
+var a;
+a = 7;
+// Only change code below this line
+
+//challenge 4
+// Setup
+var a;
+a = 7;
+var b;
+b = a;
+// Only change code below this line
+
+//challenge 5
+var a = 9;
+
+//challenge 6
+var myFirstName = "Halina";
+var myLastName = "kls';
+
+//challenge 7
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//challenge 8
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//challenge 9
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//challenge 10
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//challenge 11
+const sum = 10 + 10;
+
+//challenge 12
+const difference = 45 - 33;
+
+//challenge 13
+const product = 8 * 10;
+
+//challenge 14
+const quotient = 66 / 33;
+
+//challenge 15
+let myVar = 87;
+// Only change code below this line
+ myVar ++;
+
+//challenge 16
+let myVar = 11;
+// Only change code below this line
+myVar--;
+
+//challenge 17
+const ourDecimal = 5.7;
+// Only change code below this line
+const myDecimal = 5.7;
+
+//challenge 18
+const product = 2.0 * 2.5;
+
+//challenge 19
+const quotient = 4.4 / 2.0; // Change this line
+
+//challenge 21
+const remainder = 11 % 3;
+
+//challenge 22
+let a = 3;
+let b = 17;
+let c = 12;
+// Only change code below this line
+a += 12;
+b += 9 ;
+c += 7;
+
+//challenge 23
+let a = 11;
+let b = 9;
+let c = 3;
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+//challenge 24
+let a = 5;
+let b = 12;
+let c = 4.6;
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+//challenge 25
+let a = 48;
+let b = 108;
+let c = 33;
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+//challenge 26
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+//challenge 27
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//challenge 28
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+//challenge 29
+const myStr = "This is the start. " + "This is the end."; // Change this line
+
+//challenge 30
+let myStr = "";
+ myStr += "This is the first sentence. " + "This is the second sentence.";
+
+//challenge 31
+// Only change code below this line
+const myName = "Halina";
+const myStr = "My name is " + myName + " and I am well!";
+
+//challenge 32
+// Change code below this line
+const someAdjective = "hard";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+//challenge 33
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+// Only change code below this line
+lastNameLength = lastName.length;
+
+//challenge 34
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+//challenge 35
+// Setup
+let myStr = "Jello World";
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+//challenge 36
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+//challenge 37
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length -1]; // Change this line
+
+//challenge 38
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+//challenge 39
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+// Only change code below this line
+const wordBlanks = "The " + myAdjective + " " + myNoun + " started to " + myVerb + " away from the owner too " + myAdverb; // Change this line
+// Only change code above this line
+
+//45%
+//challenge 41
+// Only change code below this line
+const myArray = ['big', 12];
+
+//challenge 42
+// Only change code below this line
+const myArray = [[11, 21, 23],['BILD', 'house', 32]];
+
+//challenge 43 
+const myArray = [50, 60, 70];
+const myData = myArray[0]
+
+//challenge 44 (Modify Array Data With Indexes)
+// Setup
+const myArray = [18, 64, 99];
+myArray[0] = 45;
+// Only change code below this line
+
+//challenge 45
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+const myData = myArray[2][1];
+
+//challenge 46
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+// Only change code below this line
+myArray.push(["dog", 3]);
+
+//challenge 47
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+// Only change code below this line
+const removedFromMyArray = myArray.pop();
+
+//challenge 48
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+// Only change code below this line
+const removedFromMyArray = myArray.shift();
+
+//challenge 49
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
+
+//challenge 50
+const myList = [["milk", 1],
+["solt", 1],
+["meet", 2], 
+["fish", 3],
+["bread", 2]];
+
+
