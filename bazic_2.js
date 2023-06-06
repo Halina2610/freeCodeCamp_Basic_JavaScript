cbb6792a84c55c71b9fb0c6ed3e4a33c9578bf4e
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-06-07 00:28:06 +0300

    Update bazic_2.js

--- bazic_2.js
+++ bazic_2.js
@@ -341,7 +341,8 @@ testSize(7);
 
 // challenge 77
 //Гольф-код
-//В игре в гольф у каждой лунки есть par, что означает среднее количество очков, которое strokesигрок в гольф должен сделать, чтобы загнать мяч в лунку и завершить игру. В зависимости от того, насколько выше или ниже parвы strokesнаходитесь, существует другое прозвище.
+//В игре в гольф у каждой лунки есть par, что означает среднее количество очков, которое strokesигрок в гольф должен сделать, чтобы загнать мяч в лунку и завершить игру. 
+//В зависимости от того, насколько выше или ниже parвы strokesнаходитесь, существует другое прозвище.
 //Вашей функции будут переданы parи strokes аргументы. Верните правильную строку в соответствии с этой таблицей, в которой штрихи перечислены в порядке приоритета; сверху (самый высокий) к низу (самый низкий):
 const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 
@@ -373,13 +374,169 @@ golfScore(5, 4);
 Оператор switchсравнивает значение с операторами case , которые определяют различные возможные значения. 
 Любые действительные операторы JavaScript могут выполняться внутри блока case и будут выполняться с первого 
 совпадающего caseзначения до тех пор, пока breakне встретится a.*/
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case 1:
+    answer = "alpha";
+    break;
+    case 2:
+    answer = "beta";
+    break;
+    case 3:
+    answer = "gamma";
+    break;
+    case 4:
+    answer = "delta";
+    break;
+    }
+  // Only change code above this line
+  return answer;
+}
+caseInSwitch(1);
+
+//Или так:
+function caseInSwitch(val) {
+  switch (val) {
+    case 1:
+      return "alpha";
+      break;
+    case 2:
+      return "beta";
+      break;
+    case 3:
+      return "gamma";
+      break;
+    case 4:
+      return "delta";
+      break;
+  }
+}
+// Change this value to test
+caseInSwitch(1);
+
 
 // challenge 79
+//Добавление параметра по умолчанию в операторы Switch
+//В switchоператоре вы не сможете указать все возможные значения в качестве caseоператоров. 
+//Вместо этого вы можете добавить defaultоператор, который будет выполняться, если соответствующие caseоператоры не будут найдены. 
+//Думайте об этом как о последнем elseутверждении в if/elseцепочке.
+//Заявление defaultдолжно быть последним случаем.
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case 'a': // численное или текстовое значение
+    answer = "apple";
+    break;
+    case 'b':
+    answer = "bird";
+    break;
+    case 'c':
+    answer = "cat";
+    break;
+     default:
+    answer = "stuff";
+    break;
+    }
+  // Only change code above this line
+  return answer;
+}
+switchOfStuff(1);
 
 // challenge 80
+//Несколько идентичных опций в операторах switch
+/*Если breakоператор опущен в switchоператоре case, следующие caseоператоры выполняются до тех пор, пока breakне встретится a. 
+Если у вас есть несколько входов с одним и тем же выходом, вы можете представить их в switchтаком выражении*/
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2: //диапазон
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+  }
+  // Only change code above this line
+  return answer;
+}
+// Change this value to test
+sequentialSizes(1);
+
 // challenge 81
+//Замена цепочек If Else на Switch
+//Если у вас есть много вариантов на выбор, switchнаписать оператор может быть проще, чем множество связанных операторов if/ else if.
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case "bob":
+  answer = "Marley";
+  break;
+  case 42:
+  answer = "The Answer";
+  break;
+  case 1:
+  answer = "There is no #1";
+  break;
+  case 99:
+  answer = "Missed me by this much!";
+  break;
+  case 7:
+  answer = "Ate Nine";
+  break;
+  default:
+  answer = "";
+}
+  // Only change code above this line
+  return answer;
+}
+chainToSwitch(7);
+
 // challenge 82
+//Возврат логических значений из функций
+function isLess(a, b) {
+    return a < b;
+}
+isLess(10, 15);
+//вышеизложенное эквивалентно функции: 
+function isLess(a, b) {
+  // Only change code below this line
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+  // Only change code above this line
+}
+isLess(10, 15);
+
 // challenge 83
+//Вернуть ранний шаблон для функций
+//Когда returnоператор достигнут, выполнение текущей функции останавливается, и управление возвращается в место вызова.
+// Setup
+function abTest(a, b) {
+  // Измените функцию abTestтак, чтобы, если aили bменьше, 0функция немедленно завершалась со значением undefined.
+if (a < 0 || b < 0) {
+  return undefined;
+} 
+  // Only change code above this line
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+abTest(2,2);
+
 // challenge 84
 // challenge 85
 // challenge 86
