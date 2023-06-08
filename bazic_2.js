28a5c620aff0d597b297f4ab9773561cf0abb784
Halina Klyashtornaya <128702688+Halina2610@users.noreply.github.com>
2023-06-09 00:16:37 +0300

    Update bazic_2.js

--- bazic_2.js
+++ bazic_2.js
@@ -879,7 +879,44 @@ value- строка, содержащая информацию, использу
 Во-первых, если у альбома нет tracksсвойства, назначьте ему пустой массив. Затем добавьте valueпоследний элемент в массив альбома tracks.
 Примечание.recordCollection Для тестов используется копия объекта . Вы не должны напрямую изменять recor
 dCollectionобъект.*/
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
 
+// Only change code below this line
+function updateRecords(records, id, prop, value) { 
+  if (value === "") { //если значение пустое
+    delete records[id][prop]; // удаляется строка
+  } else if (prop !== "tracks" && value !== "") { //если нети valueне является пустой строкой, назначь
+    records[id][prop] = value;           //то устанавливает значение valueэтому альбому 
+  } else if (prop === "tracks" && value !== "") { 
+    if (records[id].hasOwnProperty("tracks") === false) {
+      records[id][prop] = [];
+    } //обновление
+    records[id][prop].push(value); //добавляет 
+  }
+  
+  return records; //возврат
+
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
 
 // challenge 98
 // challenge 99
